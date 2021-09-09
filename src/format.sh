55b0f2ce796e159af9018fc56ca86358ef23ffa6
kurbster <kkuznia28@gmail.com>
2021-09-09 14:50:10 -0700

    First commit

--- src/format.sh
+++ src/format.sh
@@ -4,7 +4,7 @@ files=(question.txt summary.txt hidden_sol.txt expert.txt)
 
 clean_problem() {
     for file in "${files[@]}"; do
-        ! [[ -f $1/$file ]] && echo "You must add the file $file to this directory $1." && error=1 || {
+        ! [[ -f $1/$file ]] && echo "ERROR. You must add the file $file to this directory $1." && error=1 || {
             trim=$(sed -e 's/-*Input-*/Input:/' -e 's/-*Output-*/Output:/' -e 's/-*Example-*/Example:/' -e 's/-*Note-*/Note:/' "$1/$file")
             tr -d "[\t\n\r]" <<< $trim > "$1/clean-$file"
         }
@@ -12,9 +12,11 @@ clean_problem() {
 }
 
 for problem in $(find .. -type d -not -path "../.git*" -mindepth 2); do
-    ! [[ -f $problem/clean-question.txt ]] \
-        && echo "Problem $problem needs to be cleaned." \
-        && clean_problem $problem 
+    for file in "${files[@]}"; do
+        ! [[ -f $problem/clean-$file ]] \
+            && echo "Problem $problem needs to be cleaned." \
+            && clean_problem $problem
+    done
 done
 
 [[ ! -z $error ]] && echo "Fix the error then run the format.sh script again" || echo "All problems cleaned."
--- src/test.sh
+++ src/test.sh
@@ -6,7 +6,7 @@ required_files=(question.txt summary.txt hidden_sol.txt expert.txt)
 
 for problem in $(find .. -type d -not -path "../.git*" -mindepth 2); do
     for file in "${required_files[@]}"; do
-        ! [[ -f $problem/$file ]] && echo "Make sure the format.sh script runs without errors. This file didn't exist $problem/clean-$file" && error=1
+        ! [[ -f $problem/clean-$file ]] && echo "Make sure the format.sh script runs without errors. This file didn't exist $problem/clean-$file" && error=1
     done
 done
 
