21493dffb67c797e41e85e990b9a157991ffaf69
kurbster <kkuznia28@gmail.com>
2021-11-29 21:32:20 -0700

    Added scripts to prepare data for gpt training.

--- src/gpt_train.sh
+++ src/gpt_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. training.cfg
+. configs/training.cfg
 
 # Read the training params into this var
 args=$(<$TRAINING_PARAMS)
@@ -8,10 +8,10 @@ args="${args//$'\n'/ }"
 
 ! [[ -f "./_prepared.jsonl" ]] && openai tools fine_tunes.prepare_data -f $TRAIN_DATA $PREPROCESSING_MODE
 
-openai api fine_tunes.create -t "./_prepared.jsonl" -m $MODEL_NAME $args > tmp_file
-
-trained_model=$(awk -F"Uploaded model: " '{print $2}' tmp_file | tr -d "\n")
-
-# Output the name of our trained model to a file
-echo "$trained_model" >> fine_tuned_models
-rm tmp_file
+#openai api fine_tunes.create -t "./_prepared.jsonl" -m $MODEL_NAME $args > tmp_file
+#
+#trained_model=$(awk -F"Uploaded model: " '{print $2}' tmp_file | tr -d "\n")
+#
+## Output the name of our trained model to a file
+#echo "$trained_model" >> fine_tuned_models
+#rm tmp_file
