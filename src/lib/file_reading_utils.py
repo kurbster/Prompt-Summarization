c0fa206b2e467e5b491bdcc2e7bd01ef246aa7d3
abhilashreddyy <abhilashreddyy1@gmail.com>
2021-11-30 21:31:10 -0700

    fixed bug on dict generation function

--- src/lib/file_reading_utils.py
+++ src/lib/file_reading_utils.py
@@ -9,10 +9,10 @@ def get_relative_dir_path(dir_path, relative_data_path = "../../", absolute_path
 def get_result_dict(dir_list, relative_data_path = "../"):
     test_file = "test.json"
     results_file = "all_results.json"
+    result_dict = {}
     for dir_path in dir_list:
         dir_path = get_relative_dir_path(dir_path, relative_data_path)
-        result_dict = {}
-
+        
         # read files
         test_f = open(os.path.join(dir_path,test_file))
         test_paths = json.load(test_f)
@@ -21,8 +21,14 @@ def get_result_dict(dir_list, relative_data_path = "../"):
 
         #generate result dict
         for ind in range(0, len(test_paths), 2):
+            print("*** : ", test_paths[ind].split("data")[-1])
             result_dict[test_paths[ind].split("data")[-1]] = {
-                "human" : result_obj[str(ind)],
-                "original" : result_obj[str(ind+1)]
+                "human" : result_obj[str(ind)][0],
+                "original" : result_obj[str(ind+1)][0]
             }
     return result_dict   
+
+dir_list = ["/Users/Kirby_Stuff/Documents/APPS-Summary/Prompt-Summarization/data/experiments/11-29-2021/15_23", \
+    "/Users/Kirby_Stuff/Documents/APPS-Summary/Prompt-Summarization/data/experiments/11-30-2021/13_11"]
+
+print(get_result_dict(dir_list))
